dcd58ab8d1a419bae7d0e737cf7a80e71ec47d71
trro <trro@itu.dk>
2022-03-16 13:35:12 +0100

    lil cleanup

--- Game1.cs
+++ Game1.cs
@@ -26,7 +26,7 @@ namespace Swarms
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
-            }
+        }
 
         protected override void Initialize()
         {
@@ -37,19 +37,23 @@ namespace Swarms
             base.Initialize();
         }
 
-        public int getWidth(){
+        public int getWidth()
+        {
             return _screenWidth;
         }
-        public int getHeight(){
+        public int getHeight()
+        {
             return _screenHeight;
         }
-        protected void initGrid(){
-           _grid = new SquareGrid(new Vector2(25,25), new Vector2(0,0), new Vector2(_screenWidth,_screenHeight), GraphicsDevice );
+        protected void initGrid()
+        {
+            _grid = new SquareGrid(new Vector2(25, 25), new Vector2(0, 0), new Vector2(_screenWidth, _screenHeight), GraphicsDevice);
         }
-        protected void initSize(){
+        protected void initSize()
+        {
             _screenWidth = GraphicsDevice.PresentationParameters.BackBufferWidth;
             _screenHeight = GraphicsDevice.PresentationParameters.BackBufferHeight;
-            
+
         }
         //currently unused in SquareGrid class, even though we pass it as argument
         Texture2D rectTexture;
@@ -58,7 +62,7 @@ namespace Swarms
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             rectTexture = new Texture2D(GraphicsDevice, 1, 1);
-            rectTexture.SetData(new[] {Color.White});
+            rectTexture.SetData(new[] { Color.White });
             // TODO: use this.Content to load your game content here
         }
 
@@ -66,47 +70,15 @@ namespace Swarms
         {
             base.UnloadContent();
             _spriteBatch.Dispose();
-    // If you are creating your texture (instead of loading it with
-    // Content.Load) then you must Dispose of it
+            // If you are creating your texture (instead of loading it with
+            // Content.Load) then you must Dispose of it
             rectTexture.Dispose();
         }
 
         protected override void Update(GameTime gameTime)
-        {   
-            var state = Keyboard.GetState();
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || state.IsKeyDown(Keys.Escape))
-                Exit();
+        {
+            HandleKeyInput();
 
-            //debugging purposes, just wanted to know if we could get the correct slot, which we do. this could potentially be used to place things?
-            if (state.IsKeyDown(Keys.Tab)) {
-                Trace.WriteLine(_grid.getSlotFromPixel(new Vector2(Mouse.GetState().X, Mouse.GetState().Y), -Vector2.Zero));
-            }
-            if (state.IsKeyDown(Keys.LeftControl)){
-            if (state.IsKeyDown(Keys.T)){
-                var position = _grid.getSlotFromPixel(new Vector2(Mouse.GetState().X, Mouse.GetState().Y), -Vector2.Zero);
-                var posX = position.X;
-                var posY = position.Y;
-                _grid.slots[(int)posX][(int)posY] = new Tree(position);
-
-            }
-             if (state.IsKeyDown(Keys.A)){
-                var position = _grid.getSlotFromPixel(new Vector2(Mouse.GetState().X, Mouse.GetState().Y), -Vector2.Zero);
-                var posX = position.X;
-                var posY = position.Y;
-                _grid.slots[(int)posX][(int)posY] = new Agent(position);
-
-            }
-             if (state.IsKeyDown(Keys.R)){
-                var position = _grid.getSlotFromPixel(new Vector2(Mouse.GetState().X, Mouse.GetState().Y), -Vector2.Zero);
-                var posX = position.X;
-                var posY = position.Y;
-                _grid.slots[(int)posX][(int)posY] = new Obstacle(position);
-
-            }
-            if (state.IsKeyDown(Keys.LeftShift) && state.IsKeyDown(Keys.R)){
-                initGrid();
-            }
-            }
             _grid.Update(Vector2.Zero);
             // TODO: Add your update logic here
 
@@ -116,15 +88,67 @@ namespace Swarms
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
-            
+
             //change the offset here
-            
-            _grid.drawGrid(new Vector2(0,0), _spriteBatch, rectTexture);
-         
+
+            _grid.drawGrid(new Vector2(0, 0), _spriteBatch, rectTexture);
+
             base.Draw(gameTime);
 
             //TODO, look into spritebatch and drawing, i think the grid data structure is bueno, we just need to be able to draw it.
         }
-        
+
+
+
+        private void HandleKeyInput()
+        {
+
+            var state = Keyboard.GetState();
+            var position = _grid.getSlotFromPixel(new Vector2(Mouse.GetState().X, Mouse.GetState().Y), -Vector2.Zero);
+            var posX = position.X;
+            var posY = position.Y;
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || state.IsKeyDown(Keys.Escape))
+                Exit();
+
+            //debugging purposes, just wanted to know if we could get the correct slot, which we do. this could potentially be used to place things?
+            if (state.IsKeyDown(Keys.Tab))
+            {
+                Trace.WriteLine(_grid.getSlotFromPixel(new Vector2(Mouse.GetState().X, Mouse.GetState().Y), -Vector2.Zero));
+            }
+            // Below is for adding and removing stuff
+            if (state.IsKeyDown(Keys.LeftControl))
+            {
+
+                if (state.IsKeyDown(Keys.T))
+                {
+                    _grid.slots[(int)posX][(int)posY] = new Tree(position);
+                }
+
+                if (state.IsKeyDown(Keys.A))
+                {
+                    _grid.slots[(int)posX][(int)posY] = new Agent(position);
+
+                }
+
+                if (state.IsKeyDown(Keys.R))
+                {
+                    _grid.slots[(int)posX][(int)posY] = new Obstacle(position);
+
+                }
+
+                //used for clearing
+                if (state.IsKeyDown(Keys.Space))
+                {
+                    _grid.slots[(int)posX][(int)posY] = new Boardentity();
+                }
+
+                if (state.IsKeyDown(Keys.LeftShift) && state.IsKeyDown(Keys.R))
+                {
+                    initGrid();
+                }
+
+            }
+        }
+
     }
 }
