9c453c66b6fee8b43f69b25fa63561e85ec1e26d
trro <trro@itu.dk>
2022-03-23 10:39:32 +0100

    idk

--- Datatypes/Grids/SquareGrid.cs
+++ Datatypes/Grids/SquareGrid.cs
@@ -57,7 +57,7 @@ namespace Swarms.Datatypes.Grids
             
             LoadContent(_graphics);
             setBaseGrid();
-            //setRiverGrid();
+            setRiverGrid();
             gridImg = null;
 
         }
